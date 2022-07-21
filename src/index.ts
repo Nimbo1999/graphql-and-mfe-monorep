84acc69d47a50dd67bdc7383bb0f136813baeb65
Matheus Lopes <=>
2022-07-21 16:30:59 -0300

    Update datasource file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import 'reflect-metadata';
+import { Migration } from 'typeorm';
 import server from './server.js';
 import datasource from './services/datasource.js';
 
+function verifyMigrationsStatus(migrations: Migration[]) {
+    if (migrations.length) {
+        for(const mig of migrations) {
+            console.log(`=====> Migration ${mig.name}, has been applied successfully! <=====`);
+        }
+    }
+}
+
 datasource
     .initialize()
-    .then(connection => {
-        console.log({ isInitialized: connection.isInitialized });
-        console.log('🏧  Data source initialized successfully!!');
-        server.listen({ port: 8000 }).then(({ url }) => {
-            console.log(`🚀  Server ready at ${url}`);
-        });
-    })
+    .then(connection =>
+        connection.runMigrations().then(migrations => {
+            verifyMigrationsStatus(migrations);
+            console.log('🏧  Data source initialized successfully!!');
+            server.listen({ port: 8000 }).then(({ url }) => {
+                console.log(`🚀  Server ready at ${url}`);
+            });
+        }).catch(err => {
+            console.error(err);
+            console.error('Unabled to run migrations');
+        })
+    )
     .catch(err => {
         console.error('Error during Data source initialization', err);
     });
--- src/services/datasource.ts
+++ src/services/datasource.ts
@@ -3,6 +3,7 @@ import { DataSource } from 'typeorm';
 import { Category, Finance, Metadata } from '../models/entities/index.js';
 
 const { FINANCE_DB_HOST = 'localhost' } = process.env;
+const basefolder = FINANCE_DB_HOST === 'localhost' ? 'src' : 'dist';
 
 const AppDataSource = new DataSource({
     applicationName: 'graphql-backend-app',
@@ -13,8 +14,8 @@ const AppDataSource = new DataSource({
     password: '1234',
     database: 'finance',
     entities: [Category, Finance, Metadata],
-    migrations: ['src/migrations/*.js'],
-    subscribers: ['src/subscribers/*.js'],
+    migrations: [`${basefolder}/migrations/*.js`],
+    subscribers: [`${basefolder}/subscribers/*.js`],
     migrationsTableName: 'finance_migrations',
     migrationsRun: true,
     synchronize: false,
